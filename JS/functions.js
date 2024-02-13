956728207b86a97ca28baadc13c6f78e677fa8ce
hamalainenhanna <n3haha00@students.oamk.fi>
2024-02-13 19:08:17 +0200

    Html ja  JS tehty

---
+++ JS/functions.js
@@ -0,0 +1,19 @@
+
+const list = document.querySelector('table')
+const raffle = document.querySelector('p')
+const button = document.querySelector('button')
+let valmiit_rivit = 0
+
+button.addEventListener('click',() => {
+    const tr = list.insertRow()
+    
+    for (let i = 0; i < 7; i++){
+        const td = tr.insertCell(i)
+        td.innerHTML = Math.floor(Math.random() *10)
+        console.log(i)
+    }
+    
+valmiit_rivit = valmiit_rivit +1
+document.querySelector('#rivit').innerHTML = valmiit_rivit   
+})
+
